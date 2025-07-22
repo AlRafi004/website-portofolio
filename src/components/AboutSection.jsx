5043f09a39ca4ef388aaf343f10770a8c4a3f16e
Al Rafi <rafijakson978@gmail.com>
2025-07-22 15:18:09 +0700

    Fix: Perbaiki link CV dan struktur JSX pada AboutSection

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -37,7 +37,9 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1G_8ldOqTuzHvjnd57ZyWbl0I-X6MPF2Z/view?usp=sharing"
+                href="https://drive.google.com/file/d/1zDddWACG4R3rgXvq3vZrmb6cPNx7JqHy/view?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
