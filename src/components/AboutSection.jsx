527c16f02e4111939b8a5e6549e345accd80eced
M. Hadianur Al Rafi <rafijakson978@gmail.com>
2026-01-02 20:29:21 +0700

    update link

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -37,7 +37,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1zDddWACG4R3rgXvq3vZrmb6cPNx7JqHy/view?usp=sharing"
+                href="https://drive.google.com/file/d/151-HQUfGbaxraVQP2KEdxYDnVWgn-NC-/view?usp=drive_link"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
